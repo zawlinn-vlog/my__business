71b7f908d5f0a26c2e2df141f481f7836d4cf308
zawlinn-vlog <zawlinn.profile@outlook.com>
2025-07-08 09:47:16 +0700

    myBusiness - fontend panel

---
+++ 01 Frontend Panel/assets/js/abstract/app.js
@@ -0,0 +1,7 @@
+"use script";
+
+export default class App {
+  constructor() {
+    console.log("Hello! This is Testing");
+  }
+}
---
+++ 01 Frontend Panel/assets/js/index.js
@@ -0,0 +1,6 @@
+"use script";
+
+import App from ("./abstract/app.js");
+
+const app = new App();
+console.log("hello world!");
